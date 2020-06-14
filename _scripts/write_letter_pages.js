f1b43801ec0a7e6fc0ba073f9619db14982dda5a
Mohammad H. Sattarian <mh_sattarian@outlook.com>
2020-06-14 23:27:53 +0430

    Add stand-alone page for each letter and search-bar

---
+++ _scripts/write_letter_pages.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+letters = letters.substring(1);
+
+for (letter of letters) {
+  const content = `
+  <link rel="stylesheet" href="/css/index.css" />
+
+{%- for word in collections.${letter} -%}
+  <details>
+    <summary><a href="{{word.url}}">{{ word.data.word }}</a></summary>
+    {{word.templateContent | safe}}
+    <!-- <p>Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international pavilions, award-winning fireworks and seasonal special events.</p> -->
+  </details>
+{%- endfor -%}
+    
+`;
+  fs.writeFileSync(`${letter}/${letter}.njk`, content);
+}
