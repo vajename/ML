2b9f3c7f0582e03132880ae3e0a6c06573b79ad2
Mohammad H. Sattarian <mh_sattarian@outlook.com>
2021-12-08 02:20:57 +0330

    Add elasticlunr search, and Add footer

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,17 @@
+const searchFilter = require("./words/_includes/filters/searchFilter");
+
 module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy({ assets: "assets" });
   eleventyConfig.addPassthroughCopy({ admin: "admin" });
+
+  /// Search indexing
+  eleventyConfig.addFilter("search", searchFilter);
+
+  eleventyConfig.addCollection("words_to_index", collection => {
+    return [...collection.getFilteredByGlob("./words/**/*.md")];
+  });
+
 
   return {
     passthroughFileCopy: true,
