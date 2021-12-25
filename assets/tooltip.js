92f7b1de7cfb55a223e5c3ad61c9bc60ecddedcb
Mohammad H. Sattarian <mh_sattarian@outlook.com>
2021-12-26 01:32:29 +0330

    Fix tooltips

--- assets/tooltip.js
+++ assets/tooltip.js
@@ -1,61 +1,15 @@
-// import $ from 'https://cdn.skypack.dev/cash-dom'; 
-import { computePosition, offset } from 'https://cdn.skypack.dev/@floating-ui/dom@0.1.7';
+import tippy from 'https://cdn.skypack.dev/tippy.js';
 
-const tooltip = document.createElement('div');
-tooltip.id = 'tooltip';
-tooltip.setAttribute('role', 'tooltip');
+tippy('[data-tooltip]', {
+  content(reference) {
+    const title = reference.dataset.tooltip;
+    return title;
+  },
+  placement: 'top-start',
+  arrow: true,
+  theme: 'mlgl',
 
-const tooltipArrow = document.createElement('div');
-tooltipArrow.id = 'arrow';
-
-const text = document.createElement('span');
-text.id = 'text';
-
-tooltip.append(tooltipArrow, text);
-document.body.appendChild(tooltip);
-
-const elementsWithTooltip = document.querySelectorAll('[data-tooltip]');
-
-const showEvents = ['mouseenter', 'focus'];
-const hideEvents = ['mouseleave', 'blur'];
-elementsWithTooltip.forEach(element => {
-  /// for debug:
-  // showTooltip(element)
-  showEvents.forEach((e) =>
-    element.addEventListener(e, showTooltip.bind(null, element))
-  );
-  hideEvents.forEach((e) =>
-    element.addEventListener(e, hideTooltip)
-  );
+  /// debug:
+  // hideOnClick: false,
+  // trigger: 'click',
 });
-
-let hideTimeout = undefined;
-
-function showTooltip(element) {
-  tooltip.classList.add('show');
-  text.textContent = element.dataset.tooltip;
-  updateTooltip(element);
-  clearTimeout(hideTimeout);
-}
-
-function hideTooltip() {
-  tooltip.classList.remove('show');
-  hideTimeout = setTimeout(() => {
-    text.textContent = '';
-  }, 300);
-}
-
-
-function updateTooltip(element) {
-  computePosition(element, tooltip, {
-    placement: 'top-start',
-    middleware: [
-      offset(10),
-    ],
-  }).then(({ x, y, placement, middlewareData }) => {
-    Object.assign(tooltip.style, {
-      left: `${x}px`,
-      top: `${y}px`,
-    });
-  });
-}
